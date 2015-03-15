8487dd8916fcac4a13812106a9dc1be5591b175a
Nicolas Pineda <Nicolas Pineda>
2015-03-15 02:39:53 -0600

    updating keys

--- venv/flaskOAuth/run.py
+++ venv/flaskOAuth/run.py
@@ -3,7 +3,7 @@ from project import app
 
 
 if __name__ == '__main__':
-	app.run()
-    #app.run(host='0.0.0.0', ssl_context=('tmp/cert.crt', 'tmp/key.key'), port=5000)
-   	#app.run(host='0.0.0.0', debug=True, port=1025, ssl_context=('tmp/cert.crt','tmp/key.key'))
+	app.run(host='0.0.0.0', port=8080)
+    #app.run('0.0.0.0', debug=True, port=8080, ssl_context=('tmp/cert.crt', 'tmp/key.key'))
+
 
